146822754212e5eeb5fc2cbc75e64ea09e2f574c
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-05-02 15:16:11 +0200

    cryptocurrencies component finalized

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
-import milify from 'millify';
+import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
+
 
 const { Title } = Typography;
 
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery(15);
+    const globalStats = data?.data?.stats;
+    
+
+    if(isFetching) return 'Loading...';
+
     return (
         <>
             <Title level={2} className="heading">Crypto Stats</Title>
             <Row>
-                <Col span={12}><Statistic</Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 15 Cryptoes in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show them all</Link></Title>
+            </div>
+            <Cryptocurrencies simplified />
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest news in the crypto sphere</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show them all</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 }
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f2e5e3b4efmsh454f529d222575bp110753jsn858033462674'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
