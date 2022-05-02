5c8f54dee72a4be32ea46e83b21f52b8da5da9a5
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-05-02 15:16:33 +0200

    News component temporary push

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import {Select, Typography, Row, Col, Avatar, Card} from 'antd';
+import moment from 'moment';
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const {Text, Title} = Typography;
+const {Option} = Select;
+
+const News = ({simplified}) => {
+    const { data: cryptoNews } = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12})
+    
+    if(!cryptoNews?.value) return 'Loading...';
+
     return (
-        <div>
-            News
-        </div>
+        <Row gutter={[24, 24]}>
+            {cryptoNews.value.map((news, i) => (
+                <Col xs={24} sm={12}  lg={8} key={i}> 
+                    <Card hoverable style={{border: "1px solid blue"}}>
+
+                    </Card>
+                    {/* <Card hoverable className="news-card">
+                        <a href={news.url} target="_blank" rel="noreferrer">
+                            <div className="news-image-container">
+                                <Title className="news-title" level={4}>{news.title}</Title>
+                            </div>
+                        </a>
+                    </Card> */}
+                </Col>
+            ))}
+        </Row>
     )
 }
 
