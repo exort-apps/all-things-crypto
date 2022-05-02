9f715e05d06b221e1156a8a81aa03788eb91e7e8
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-05-03 00:11:17 +0200

    News component finished

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,31 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Select, Typography, Row, Col, Avatar, Card} from 'antd';
 import moment from 'moment';
+import demoNewsImage from '../images/demoImageCrypto.jpg';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const {Text, Title} = Typography;
 const {Option} = Select;
 
 const News = ({simplified}) => {
-    const { data: cryptoNews } = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12})
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency'); 
+    const { data: cryptoNews } = useGetCryptoNewsQuery({newsCategory, count: simplified ? 6 : 12})
+    const { data } = useGetCryptosQuery(100);
     
     if(!cryptoNews?.value) return 'Loading...';
 
     return (
         <Row gutter={[24, 24]}>
+
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Select crypto news"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase())}
+                    >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
+                    </Select>
+                </Col>
+            )}
+
             {cryptoNews.value.map((news, i) => (
                 <Col xs={24} sm={12}  lg={8} key={i}> 
-                    <Card hoverable style={{border: "1px solid blue"}}>
-
-                    </Card>
-                    {/* <Card hoverable className="news-card">
+                    <Card hoverable className="news-card">
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
-                                <Title className="news-title" level={4}>{news.title}</Title>
+                                <Title className="news-title" level={5}>{news.name}</Title>
+                                <img src={news?.image?.thumbnail?.contentUrl || demoNewsImage } alt="news" style={{maxWidth:'200px', maxHeight:'100px'}}/> 
+                            </div>
+                            <p>
+                                {news.description > 100
+                                    ? `${news.description.substring(0, 100)}`
+                                    : news.description
+                                }
+                            </p>
+                            <div className="provider-container">
+                                <div>
+                                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoNewsImage} alt="news provider avatar" />
+                                    <Text className="provider-name">{news.provider[0]?.name}</Text>
+                                </div>
+                                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                             </div>
                         </a>
-                    </Card> */}
+                    </Card>
                 </Col>
             ))}
         </Row>
