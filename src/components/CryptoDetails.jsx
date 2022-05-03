cdc1ff71d24d39e49ab79be91a30eb60c2bff5c4
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-05-03 21:02:08 +0200

    fixed minor error

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -5,13 +5,17 @@ import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+
 const { Title, Text } = Typography;
 const { Option } = Select;
 
-
-
 const CryptoDetails = () => {
     const { uuid } = useParams();
+    const [timePeriod, setTimePeriod] = useState('7d')
+    const { data, isFetching } = useGetCryptoDetailsQuery(uuid);
+
+    console.log(data);
 
     return (
         <div>
