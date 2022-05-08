bb5937db8c732976714c0028143a552c29e7b3f9
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-05-08 10:51:02 +0200

    Properly showing dates

--- src/components/ChartLine.jsx
+++ src/components/ChartLine.jsx
@@ -9,12 +9,22 @@ const ChartLine = ({coinHistory, currentPrice, coinName}) => {
     const coinPrice = []
     const coinTimestamp = []
 
-    for(let i=0; i < coinHistory?.data?.history?.length;i+=1) {
-        coinPrice.push(coinHistory?.data?.history[i]?.price)
+    const len =  coinHistory?.data?.history?.length
+    const ylen =  coinHistory?.data?.history?.length
+
+    for(let i = len-1;i>0;i--) {
+        coinPrice.push(coinHistory?.data?.history[i].price)
     }
-    for(let i=0; i < coinHistory?.data?.history?.length;i+=1) {
-        coinTimestamp.push(new Date(coinHistory?.data?.history[i]?.timestamp).toLocaleDateString())
+    for(let i = ylen-1;i>0;i--) {
+        coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp*1000).toLocaleDateString("en-US"))
     }
+    // This loop was working but not showing the correct dates in the xAxis of the chart
+    // for(let i=0; i < coinHistory?.data?.history?.length;i+=1) {
+    //     coinPrice.push(coinHistory?.data?.history[i]?.price)
+    // }
+    // for(let i=0; i < coinHistory?.data?.history?.length;i+=1) {
+    //     coinTimestamp.push(new Date(coinHistory?.data?.history[i]?.timestamp).toLocaleDateString())
+    // }
 
     const data = {
         labels: coinTimestamp,
