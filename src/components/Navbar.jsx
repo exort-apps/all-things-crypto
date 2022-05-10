d847d3be8a0c5c9fd87cf1444e99e791fbc30e09
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-05-10 20:42:56 +0200

    logo changed to rectangular shape

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo-container">
-                <Avatar src={icon} size="large" />
+                <Avatar src={icon} shape="square" size="large" />
                 <Typography.Title level={2} className="logo">
                     <Link to="/">All Things Crypto</Link>
                 </Typography.Title>
